2924d24fcc986e847540ccb27edec715a8a0db3c
Paulina Lei <paulina.y.lei@gmail.com>
2017-05-09 18:11:44 -0700

    menu, cards for prettier content

--- js/template.js
+++ js/template.js
@@ -16,3 +16,22 @@ function articles(index) {
     }
   });
 }
+
+function showMenu() {
+    document.getElementById("menu").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
